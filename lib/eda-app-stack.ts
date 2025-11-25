0a792f9a1269e097c955a48fd39f9b19a4a2cbc0
finn <finnbennett2002@gmail.com>
2025-11-25 12:23:57 +0000

    Change mailer trigger to the db stream

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -9,6 +9,7 @@ import * as sns from "aws-cdk-lib/aws-sns";
 import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as source from "aws-cdk-lib/aws-lambda-event-sources";
 
 
 import { Construct } from "constructs";
@@ -30,6 +31,7 @@ export class EDAAppStack extends cdk.Stack {
       partitionKey: { name: "name", type: dynamodb.AttributeType.STRING },
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       tableName: "Imagess",
+      stream: dynamodb.StreamViewType.NEW_IMAGE 
  });
 
 
@@ -55,11 +57,6 @@ export class EDAAppStack extends cdk.Stack {
        const newImageTopic = new sns.Topic(this, "NewImageTopic", {
       displayName: "New Image topic",
     }); 
-
-
-        const mailerQ = new sqs.Queue(this, "mailer-q", {
-      receiveMessageWaitTime: cdk.Duration.seconds(10),
-    });
 
 
 
@@ -132,25 +129,6 @@ export class EDAAppStack extends cdk.Stack {
       })
  );
 
-    newImageTopic.addSubscription(
-      new subs.SqsSubscription(mailerQ, {
-        filterPolicyWithMessageBody: {
-          Records: sns.FilterOrPolicy.policy({
-            s3: sns.FilterOrPolicy.policy({
-              object: sns.FilterOrPolicy.policy({
-                key: sns.FilterOrPolicy.filter(
-                  sns.SubscriptionFilter.stringFilter({
-                    matchPrefixes: ["image"],
-                  })
-                ),
-              }),
-            }),
-           }),
-         },
-        rawMessageDelivery: true,
-      })
- );
-
 
     newImageTopic.addSubscription(
       new subs.LambdaSubscription(addMetadataFn, {
@@ -170,11 +148,6 @@ export class EDAAppStack extends cdk.Stack {
       batchSize: 5,
       maxBatchingWindow: cdk.Duration.seconds(5),
     });
-
-     const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
-      batchSize: 5,
-      maxBatchingWindow: cdk.Duration.seconds(5),
-    }); 
 
 
     const rejectedImageEventSource = new events.SqsEventSource(dlq, {
@@ -199,8 +172,13 @@ export class EDAAppStack extends cdk.Stack {
 
 
     processImageFn.addEventSource(newImageEventSource);
-    mailerFn.addEventSource(newImageMailEventSource);
-     rejectedImageFn.addEventSource(rejectedImageEventSource);
+    rejectedImageFn.addEventSource(rejectedImageEventSource);
+
+     mailerFn.addEventSource(
+      new source.DynamoEventSource(imagesTable, {
+        startingPosition: lambda.StartingPosition.LATEST
+ })
+ )
 
 
     // Permissions
